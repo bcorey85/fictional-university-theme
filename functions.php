52f193f238f21c7bd1c7aaf53f7b08f2f7576cbe
Brandon Corey <bcorey85@gmail.com>
2020-11-03 07:38:04 -0600

    add events to front page

--- functions.php
+++ functions.php
@@ -1,10 +1,17 @@
 <?php
 
 function university_files() {
-  wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), null, '1.0', true);
   wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-  wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+
+  if(strstr($_SERVER['SERVER_NAME'], 'fictionaluniversity.local')) {
+    wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', null, '1.0', true);
+  } else {
+    wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.8c97d901916ad616a264.js'), null, '1.0', true);
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.361ae884830d52b8ba98.js'), null, '1.0', true);
+    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.361ae884830d52b8ba98.css'));
+  }
+  
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
@@ -15,3 +22,5 @@ function university_features() {
 
 add_action('after_setup_theme', 'university_features');
 
+
+?>
